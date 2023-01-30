f337bdbdba499be1be60cc3a38f2e3e0bbd89ea5
FlagiH <neftimognat@mail.ru>
2023-01-30 14:14:16 +0400

    js container__button-read-more

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
-// var mySwiper = new Swiper ('.swiper-container', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true
 const swiper = new Swiper('.swiper', {
   // Default parameters
   slidesPerView: 'auto',
   allowTouchMove: true, 
   // Responsive breakpoints
   breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 1,
-  //   },
-  //   // when window width is >= 480px
     768: {
       enabled: false
     },
-  //   // when window width is >= 640px
-  //   1120: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 32
-  //   }
   },
   pagination: {
     el: '.swiper-pagination',
@@ -28,5 +14,28 @@ const swiper = new Swiper('.swiper', {
   },
 })
 
+const readMoreButton = document.querySelector('.container__button-read-more');
+const cuttedBlock = document.querySelector('.container__block--cut');
 
+readMoreButton.addEventListener('click', function () {
+  // if container block has class 'container__block--cut'
+    // 1. reset max-height to show content  max-height: initial;
+    // 2. rotate before arrows; 
+    // 3. change text 'from read' more to 'hide'
+  // else
+    // 1. set max-height to 186px
+    // 2. reset before arrows
+    // 3. change text from 'hide' to 'read more'
 
+  if (cuttedBlock.classList.contains('container__block--cut')) {
+    cuttedBlock.classList.remove('container__block--cut');
+    readMoreButton.classList.add('container__button-read-more--rotated')
+    readMoreButton.textContent = 'Скрыть'; 
+  } else {
+    cuttedBlock.classList.add('container__block--cut');
+    readMoreButton.classList.remove('container__button-read-more--rotated')
+    readMoreButton.textContent = 'Показать все';
+  }
+})
+
+console.log(cuttedBlock);
